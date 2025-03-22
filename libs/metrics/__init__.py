2f005b1542351404cbfdf6052b358dbad81d5462
carolina-kelsch-isi <carolina.kelsch@senairs.org.br>
2025-03-22 16:56:07 -0300

    update metrics import

--- libs/metrics/__init__.py
+++ libs/metrics/__init__.py
@@ -2,3 +2,4 @@ from libs.metrics.base import *
 from libs.metrics.image_and_text import *
 from libs.metrics.image_and_image import *
 from libs.metrics.fid import *
+from libs.metrics.classification_accuracies import *
