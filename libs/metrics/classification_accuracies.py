08d3d6a79e63d51c0375d86eaea5a2c770a43572
Carolina Kelsch <48366606+carolkelsch@users.noreply.github.com>
2025-04-01 20:57:59 -0300

    fix classification accuracy

--- libs/metrics/classification_accuracies.py
+++ libs/metrics/classification_accuracies.py
@@ -1,26 +1,22 @@
 from typing import List, Literal, Dict, Optional
-from torchvision import transforms
-from libs.metrics.base import Metric
 import numpy as np
 import torch
-import torchvision.models as models
-from torch.utils.data import DataLoader
+from libs.metrics.base import Metric
 from libs.datasets import UnlearnDatasetSplit
 
 
 class ClassificationAccuracy(Metric):
-    metrics: List[Literal['Unlearn', 'Remaining', 'Testing']]
-    unlearned_model: models
-    loaders: Dict[str, DataLoader]
+    metrics: List[Literal['Unlearn', 'Unlearn_test', 'Remaining', 'Testing']]
+    outputs: Dict[str, torch.Tensor]
+    targets: Dict[str, torch.Tensor]
 
     def model_post_init(self, __context: dict = None) -> None:
-        # Download the models, if required
-        expected_keys = [UnlearnDatasetSplit.Train_forget, UnlearnDatasetSplit.Train_retain, UnlearnDatasetSplit.Test_retain]
-        assert set(expected_keys) == set([key.value for key in list(self.loaders.keys())]), "Loaders should be filled"
-        assert isinstance(self.unlearned_model, models), "Model not valid"
+        expected_keys = [UnlearnDatasetSplit.Train_forget, UnlearnDatasetSplit.Test_forget, UnlearnDatasetSplit.Train_retain, UnlearnDatasetSplit.Test_retain]
+        assert set(key.value for key in expected_keys) == set([key for key in list(self.outputs.keys())]), "Outputs should be filled"
+        assert set(key.value for key in expected_keys) == set([key for key in list(self.targets.keys())]), "Targets should be filled"
         pass
 
-    def _accuracy(output: torch.Tensor, target: torch.Tensor, topk: tuple = (1,)) -> list:
+    def _accuracy(self, output: torch.Tensor, target: torch.Tensor, topk: tuple = (1,)) -> list:
         """
         Computes the accuracy over the top-k predictions for the specified values of k.
         
@@ -32,62 +28,33 @@ class ClassificationAccuracy(Metric):
         Returns:
             list: Accuracy values for each k in topk.
         """
-        with torch.no_grad():
-            maxk = max(topk)  # Get the highest k value
-            batch_size = target.size(0)
+        maxk = max(topk)  # Get the highest k value
+        batch_size = target.size(0)
 
-            # Get the top-k predictions
-            _, pred = output.topk(maxk, dim=1, largest=True, sorted=True)  # (batch_size, maxk)
-            pred = pred.t()  # Transpose to shape (maxk, batch_size)
+        # Get the top-k predictions
+        _, pred = output.topk(maxk, dim=1, largest=True, sorted=True)  # (batch_size, maxk)
+        pred = pred.t()  # Transpose to shape (maxk, batch_size)
 
-            # Compare predictions with targets
-            correct = pred.eq(target.view(1, -1).expand_as(pred))  # Shape (maxk, batch_size)
+        # Compare predictions with targets
+        correct = pred.eq(target.view(1, -1).expand_as(pred))  # Shape (maxk, batch_size)
 
-            # Compute accuracy for each k
-            accuracies = []
-            for k in topk:
-                correct_k = correct[:k].reshape(-1).float().sum(0)  # Sum up correct predictions
-                acc_k = correct_k * 100.0 / batch_size  # Convert to percentage
-                accuracies.append(acc_k.item())
+        # Compute accuracy for each k
+        accuracies = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0)  # Sum up correct predictions
+            acc_k = correct_k * 100.0 / batch_size  # Convert to percentage
+            accuracies.append(acc_k.item())
 
         return accuracies
 
-    def _compute_acc(self, metric, loader: DataLoader, unlearn: bool = False) -> float:
-        losses = np.zeros(len(loader))
-        top1 = np.zeros(len(loader))
+    def _compute_acc(self, output: torch.Tensor, target: torch.Tensor, unlearn: bool = False) -> float:
         
-        criterion = torch.nn.CrossEntropyLoss()
-
-        # switch to evaluate mode
-        self.unlearned_model.eval()
-        device = (torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"))
-
-        for i, (image, target) in enumerate(loader):
-
-            image = image.cuda()
-            target = target.cuda()
-
-            with torch.no_grad():
-                output = self.unlearned_model(image)
-                loss = criterion(output, target)
-
-            output = output.float()
-            loss = loss.float()
-
-            prec1 = self._accuracy(output.data, target)[0]
-            losses[i] = loss.item()
-            top1[i] = prec1.item()
-
-            print(f"Test: [{i}/{len(loader)}]\t" + 
-            f"Loss {losses[i]:.4f} ({np.mean(losses):.4f})\t" +
-            f"Accuracy {top1[i]:.3f} ({np.mean(top1):.3f})")
-
-            print(f"Accuracy {np.mean(top1):.3f}")
+        top1 = self._accuracy(output.data, target)[0]
         
-        if unlearn == True:
-            return (1 - np.mean(top1))
-        else:
-            return np.mean(top1)
+        if unlearn is True:
+            return (100 - np.mean(top1))
+        
+        return np.mean(top1)
 
     def score(self) -> Dict[str, float]:
         scores: Dict[str, float] = {}
@@ -95,12 +62,14 @@ class ClassificationAccuracy(Metric):
         for metric in self.metrics:
             if metric == 'Unlearn':
                 # TODO: change to test_forget, right now using train to be equal to SalUn
-                self.scores[metric] = self._compute_acc(metric, self.loaders[UnlearnDatasetSplit.Train_forget], True)
-            if metric == 'Remaining':
-                self.scores[metric] = self._compute_acc(metric, self.loaders[UnlearnDatasetSplit.Train_retain])
+                scores[metric] = self._compute_acc(self.outputs[UnlearnDatasetSplit.Train_forget.value], self.targets[UnlearnDatasetSplit.Train_forget.value], True)
+            elif metric == 'Unlearn_test':
+                scores[metric] = self._compute_acc(self.outputs[UnlearnDatasetSplit.Test_forget.value], self.targets[UnlearnDatasetSplit.Test_forget.value], True)
+            elif metric == 'Remaining':
+                scores[metric] = self._compute_acc(self.outputs[UnlearnDatasetSplit.Train_retain.value], self.targets[UnlearnDatasetSplit.Train_retain.value])
             else:
-                self.scores[metric] = self._compute_acc(metric, self.loaders[UnlearnDatasetSplit.Test_retain])
+                scores[metric] = self._compute_acc(self.outputs[UnlearnDatasetSplit.Test_retain.value], self.targets[UnlearnDatasetSplit.Test_retain.value])
         
-        assert len(self.scores) == len(self.metrics)
+        assert len(scores) == len(self.metrics)
         
         return scores
